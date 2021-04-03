18ab2c0f9b9d58ce1a5c21ece3a526902d0c5b8c
wendirad <wendiradame@gmail.com>
2021-04-03 16:45:21 +0300

    [feat] Register `ausers` to default admin

--- src/tena/ausers/admin.py
+++ src/tena/ausers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as UA
 
-# Register your models here.
+from ausers.models import Customer, Region, Woreda, Zone
+
+
+@admin.register(get_user_model())
+class UserAdmin(UA):
+    pass
+
+admin.site.register(Region)
+admin.site.register(Zone)
+admin.site.register(Woreda)
+admin.site.register(Customer)
