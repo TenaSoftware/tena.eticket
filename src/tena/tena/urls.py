34282c2b042ea70abb5b1b0259f27ae0f8cf017f
wendirad <wendiradame@gmail.com>
2021-04-03 16:44:50 +0300

    [feat] Adding smart_selects url

--- src/tena/tena/urls.py
+++ src/tena/tena/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import include, re_path
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^chaining/', include('smart_selects.urls')),
 ]
