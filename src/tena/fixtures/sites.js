9897266c4e12b75c39820c23a85fc37d4d963ed6
wendirad <wendiradame@gmail.com>
2021-04-03 11:12:06 +0300

    [feat] Configuring common sites * It need to be different site for customers and employees for security and simplicy.

---
+++ src/tena/fixtures/sites.js
@@ -0,0 +1,20 @@
+[
+  {
+    "model": "sites.site",
+    "pk": 1,
+    "fields":
+      {
+        "domain": "127.0.0.1:8000",
+        "name": "Tena Transport"
+      }
+    },
+    {
+      "model": "sites.site",
+      "pk": 2,
+      "fields":
+        {
+          "domain": "127.0.0.1:8001",
+          "name": "Tena Management"
+        }
+      },
+  ]
