4a860b70e305ba36a2d078eb65db2c27dbd85a28
wendirad <wendiradame@gmail.com>
2021-04-18 12:24:01 +0300

    [feat] Move MessageView to pages and SendMessageView

--- src/tena/pages/views.py
+++ src/tena/pages/views.py
@@ -1,4 +1,38 @@
-from django.views.generic import TemplateView
+from django.contrib import messages
+from django.views.generic import RedirectView, TemplateView, View
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponseRedirect
+from django.utils.decorators import classonlymethod
 
 class HomePage(TemplateView):
     template_name = 'pages/index.html'
+
+class MessageView(RedirectView):
+    url = '/'
+    message = None
+    level = None
+
+    @classonlymethod
+    def as_view(cls, **initkwargs):
+        message = initkwargs.get('message', None)
+        level = initkwargs.get('level', 'SUCCESS')
+        if message is None:
+            raise ImproperlyConfigured('Message is not included in kwargs.')
+        if level.upper() not in messages.DEFAULT_LEVELS.keys():
+            raise ImproperlyConfigured('Unknown level. Level should be one of the following %s.',
+                    ', '.join(messages.DEFAULT_LEVELS.keys())
+            )
+        initkwargs.update({'level': messages.DEFAULT_LEVELS.get(level)})
+        return super().as_view(**initkwargs)
+
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(request, self.level, self.message)
+        return super().dispatch(request, *args, **kwargs)
+
+class SendMessageView(View):
+
+    def get(self, request, *args, **kwargs):
+        msg = kwargs.get('msg', None)
+        messages.add_message(request, 20, msg)
+        return HttpResponseRedirect('/')
