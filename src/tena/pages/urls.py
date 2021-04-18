3270459f6d880c6225c2cae4a70135a575dc9fbe
wendirad <wendiradame@gmail.com>
2021-04-18 23:37:00 +0300

    [feat] Add homepage url [feat] Add send message url

--- src/tena/pages/urls.py
+++ src/tena/pages/urls.py
@@ -1,10 +1,12 @@
-from django.views.generic import RedirectView
-
 from django.urls import re_path, reverse_lazy
+from django.views.generic import RedirectView
 
 from pages import views
 
 urlpatterns = [
-    re_path(r'^home/', views.HomePage.as_view(), name='home'),
-    re_path(r'', RedirectView.as_view(url=reverse_lazy('home'))),
+    re_path(r"^home/", views.HomePage.as_view(), name="home"),
+    re_path(
+        r"^msg/(?P<msg>.+)/$", views.SendMessageView.as_view(), name="send-message"
+    ),
+    re_path(r"", RedirectView.as_view(url=reverse_lazy("home"))),
 ]
