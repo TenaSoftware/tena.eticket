acfc2cf7affaca60f20bafa497583f13cb3d4748
wendirad <wendiradame@gmail.com>
2021-05-18 17:15:44 +0300

    [feat] Add initial CRUD View to models

--- src/tena/core/views.py
+++ src/tena/core/views.py
@@ -1,3 +1,110 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixins, PermissionRequiredMixin
 
-# Create your views here.
+class LPRequiredMixin(LoginRequiredMixins, PermissionRequiredMixin):
+	""" Used to include both LoginRequiredMixin and PermissionRequiredMixin
+		together. """
+	pass
+
+# Branch CRUD Views
+
+class AddBranchView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateBranchView(LPRequiredMixin, CreateView):
+	pass
+
+class ListBranchView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveBranchView(LPRequiredMixin, ListView):
+	pass
+
+
+# Bus CRUD Views
+class AddBusView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateBusView(LPRequiredMixin, CreateView):
+	pass
+
+class ListBusView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveBusView(LPRequiredMixin, ListView):
+	pass
+
+
+# Driver CRUD Views
+class AddDriverView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateDriverView(LPRequiredMixin, CreateView):
+	pass
+
+class ListDriverView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveDriverView(LPRequiredMixin, ListView):
+	pass
+
+
+# Route CRUD Views
+class AddRouteView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateRouteView(LPRequiredMixin, CreateView):
+	pass
+
+class ListRouteView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveRouteView(LPRequiredMixin, ListView):
+	pass
+
+
+# Ticket CRUD Views
+class AddTicketView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateTicketView(LPRequiredMixin, CreateView):
+	pass
+
+class ListTicketView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveTicketView(LPRequiredMixin, ListView):
+	pass
+
+
+# Receipt CRUD Views
+class AddReceiptView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateReceiptView(LPRequiredMixin, CreateView):
+	pass
+
+class ListReceiptView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveReceiptView(LPRequiredMixin, ListView):
+	pass
+
+
+# Appointement CRUD Views
+class AddAppointementView(LPRequiredMixin, CreateView):
+	pass
+
+class UpdateAppointementView(LPRequiredMixin, CreateView):
+	pass
+
+class ListAppointementView(LPRequiredMixin, ListView):
+	pass
+
+class RemoveAppointementView(LPRequiredMixin, ListView):
+	pass
+
+
+
+
+
