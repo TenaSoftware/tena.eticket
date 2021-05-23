a95d08164c752054e77f630b86fa2954383b4e18
wendirad <wendiradame@gmail.com>
2021-05-23 22:46:49 +0300

    [fix] Fix permission handling for SearchRouteView

--- src/tena/core/views.py
+++ src/tena/core/views.py
@@ -1,14 +1,19 @@
-from django.views.generic import CreateView, UpdateView, ListView, FormView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.views.generic import CreateView, FormView, ListView, UpdateView
 
 from core.forms import SearchRouteForm
+
 
 class LPRequiredMixin(LoginRequiredMixin, PermissionRequiredMixin):
 	""" Used to include both LoginRequiredMixin and PermissionRequiredMixin
 		together. """
 	pass
 
-
-class SearchRouteView(FormView):
-	form_class = SearchRouteForm
-	template_name = "core/search_route.html"
+class SearchRouteView(LPRequiredMixin, FormView):
+    form_class = SearchRouteForm
+    template_name = "core/search_route.html"
+    permission_required = ()
+    
+    def form_valid(self, form):
+        print(form.data)
